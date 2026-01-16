797a00c30959efa0451be34c39c2dca5f5b0bd7e
jesusdonate <66103218+jesusdonate@users.noreply.github.com>
2026-01-15 17:41:22 -0800

    Updated requirement.txt and modified initialize_driver function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 import pandas as pd
 import os
 import sys
@@ -93,8 +95,9 @@ def initialize_driver():
 
     try:
         if sys.platform == 'linux':
+            service = Service(ChromeDriverManager().install())
             return webdriver.Chrome(
-                service=Service("/usr/local/bin/chromedriver"),
+                service=service,
                 options=chrome_options
             )
         else:  # Windows/Mac
